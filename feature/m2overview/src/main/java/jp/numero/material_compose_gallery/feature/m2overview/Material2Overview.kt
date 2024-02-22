c2ff2af1ed1ed45583ba8c4abf46fab6ed65a4e4
NUmeroAndDev <numero.dev.and@gmail.com>
2024-02-22 21:46:36 +0900

    Added Progress Indicator item

--- feature/m2overview/src/main/java/jp/numero/material_compose_gallery/feature/m2overview/Material2Overview.kt
+++ feature/m2overview/src/main/java/jp/numero/material_compose_gallery/feature/m2overview/Material2Overview.kt
@@ -27,6 +27,7 @@ import jp.numero.material_compose_gallery.feature.m2overview.items.buttonsItem
 import jp.numero.material_compose_gallery.feature.m2overview.items.cardItem
 import jp.numero.material_compose_gallery.feature.m2overview.items.checkboxItem
 import jp.numero.material_compose_gallery.feature.m2overview.items.chipsItem
+import jp.numero.material_compose_gallery.feature.m2overview.items.progressIndicatorItem
 import jp.numero.material_compose_gallery.feature.m2overview.items.topAppBarItem
 
 @Composable
@@ -91,6 +92,7 @@ private fun Material2OverviewContent(
         cardItem()
         checkboxItem()
         chipsItem()
+        progressIndicatorItem()
         topAppBarItem()
         bottomAppBarItem()
     }
